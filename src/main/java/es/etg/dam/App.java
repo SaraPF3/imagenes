23f87471d96fadc462f23da3dda862ef5ca2a9ca
[Sara] <[sarapefe5@gmail.com]>
2025-10-23 07:43:20 -0500

    Se ha modificado el proyecto

--- src/main/java/es/etg/dam/App.java
+++ src/main/java/es/etg/dam/App.java
@@ -7,9 +7,7 @@ public class App {
     public static final int NUM0 = 0;
     public static final int NUM1 = 1;
     private static final String COMANDO = "convert";
-    private static final String MSG_ERROR = "Error: ";
     private static final String MSG_ERROR_ARGS = "Se deben introducir los valores correctamente";
-    private static final String MSG_FIN = "Conversión completada: ";
 
     public static void main(String[] args) throws IOException, Exception {
 
@@ -21,23 +19,7 @@ public class App {
         String imagenEntrada = args[NUM0];
         String imagenSalida = args[NUM1];
 
-        String[] comando = {COMANDO, imagenEntrada, imagenSalida};
-
-        try {
-            Process proceso = Runtime.getRuntime().exec(comando);
-
-            int resultado = proceso.waitFor();
-
-            if (resultado == 0) {
-                System.out.println(MSG_FIN + imagenSalida);
-            } else {
-                System.out.println(MSG_ERROR + resultado);
-            }
-
-        } catch (IOException ioe) {
-            System.out.println(MSG_ERROR + ioe);
-        } catch (InterruptedException ie) {
-            System.out.println(MSG_ERROR + ie.getMessage());
-        }
+        Ejecutable ejec = new Comando(COMANDO, imagenEntrada, imagenSalida);
+        ejec.ejecutar();
     }
 }
---
+++ src/main/java/es/etg/dam/Comando.java
@@ -0,0 +1,52 @@
+package es.etg.dam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Comando implements Ejecutable {
+
+    private String comando;
+    private String parametro;
+    private String parametro2;
+
+
+    public Comando(String comando) {
+        this.comando = comando;
+    }
+
+    @Override
+    public String ejecutar() throws IOException, Exception {
+
+        String MSG_ERROR = "Ha ocurrido un error al ejecutar el comando.";
+        String N = "\n";
+        StringBuilder output = new StringBuilder();
+
+        String[] comand = {comando, parametro, parametro2};
+
+        try {
+            Process process = Runtime.getRuntime().exec(comand);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(N);
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+            } else {
+                System.out.println(MSG_ERROR);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            System.exit(34);
+        }
+        return output.toString();
+    }
+}
